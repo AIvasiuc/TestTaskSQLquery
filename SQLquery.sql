6ddf4bc43d0a35fdcf61164c2fb24392eb0a458d
AIvasiuc <alexandr.ivasiuc@gmail.com>
2018-01-07 20:12:41 +0200

    Initial commit

---
+++ SQLquery.sql
@@ -0,0 +1,14 @@
+﻿SELECT CustomerId, RegistrationDateTime
+FROM Customers
+WHERE CustomerId IN 
+(
+SELECT DISTINCT CustomerId 
+FROM Purchases 
+WHERE ProductName = 'молоко'
+AND CustomerId NOT IN
+(
+SELECT CustomerId 
+FROM Purchases 
+WHERE ProductName = 'сметана' AND PurchaseDatetime >= DATEADD(MONTH, -1, GETDATE())
+)
+)
